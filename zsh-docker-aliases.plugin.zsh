a4b08800a9850285eadbb42b6439b1a6dcb22ec9
Henry Wang <henry@wangqiru.com>
2018-04-18 17:23:36 +0100

    Made compatible with oh-my-zsh

---
+++ zsh-docker-aliases.plugin.zsh
@@ -0,0 +1,6 @@
+# Made compatible with oh-my-zsh
+
+# Install: git clone https://github.com/akarzim/zsh-docker-aliases ~/.oh-my-zsh/custom/plugins/zsh-docker-aliases
+# Update.zshrc plugin section: plugins = ( zsh-docker-aliases )
+
+source ${0:A:h}/docker-aliases.plugin.zsh
